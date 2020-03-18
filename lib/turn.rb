95e97fc4b3c577de378875706e46a0d31ac949fe
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-18 15:25:21 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,17 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def position_taken?(board, index)
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    false
+  else
+    true
+  end
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board, index)==false
+    true
+  end
+end
